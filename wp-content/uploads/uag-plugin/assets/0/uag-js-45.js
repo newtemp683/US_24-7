29db425de22431c1b4fbf4e70cf8d450884424f2
gsquad786 <gsquadtech@gmail.com>
2025-04-06 18:08:30 +0530

    Initial commit with project files

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-45.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-eb72779f', {"layout":"number","heading":"Satisfied customers","numberPrefix":"","numberSuffix":"+","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-1387a750', {"layout":"number","heading":"Projects done","numberPrefix":"","numberSuffix":"k","startNumber":0,"endNumber":50,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
